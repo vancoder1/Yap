103dac5ba923c8c446c28b8d91182411fc644521
Ivan Zaporozhets <ivanzaporozhets25@gmail.com>
2024-06-10 13:00:39 -0600

    initial commit

---
+++ Yap/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Yap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
